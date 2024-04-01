c941cd22fe69debc20ac42555aee37758ad7ceca
matt <matt.thornfield@neueda.com>
2024-04-01 10:41:00 -0400

    container v1 almost

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { mount } from 'payments_list/Transactions';
 
 function App() {
+
+  const transactions = useRef<HTMLDivElement>(null);
+
+  useEffect( () => {
+    mount(transactions.current);
+  }, [])
+
   return (
     <div><h1>This is the container</h1>
     
-      //display transactions here
+      <div ref={transactions}></div>
     </div>
   );
 }
