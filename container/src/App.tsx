dc7ad7d166ffe06998c6dbf16ad8876b7ce5d0f8
matt <matt.thornfield@neueda.com>
2024-04-01 09:41:59 -0400

    first commit

---
+++ container/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div><h1>This is the container</h1></div>
+  );
+}
+
+export default App;
